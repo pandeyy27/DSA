df1d33a2a11ccc37432f656c9f788db7bd559c42
Shubham <shubhampandeyy27@gmail.com>
2022-12-06 15:40:40 +0530

    first commit

---
+++ 02 - Arrays/buyAndSellStocks.java
@@ -0,0 +1,25 @@
+// Best Time to Buy and Sell Stock
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+public class buyAndSellStocks {
+
+    public static int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(maxProfit(a));
+    }
+}
