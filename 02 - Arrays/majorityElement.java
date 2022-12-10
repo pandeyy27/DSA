8548322f7393038e571fe44259541996c111d124
Shubham <shubhampandeyy27@gmail.com>
2022-12-10 19:19:14 +0530

    first commit

---
+++ 02 - Arrays/majorityElement.java
@@ -0,0 +1,43 @@
+// Majority Element Problem - Find the element that repeats more than n/2 times
+// Approach: Moore's voting algorithm O(N)
+// Step 1: We find the candidate which is majority in the array
+// Step 2: We check wether the candidate is in majority or not?
+
+public class majorityElement {
+    static int output(int a[]) {
+        int n = a.length;
+        int count = 1, res = 0;
+
+        // Step 1:
+        for (int i = 1; i < n; i++) {
+            if (a[res] == a[i]) {
+                count++;
+            } else {
+                count--;
+            }
+
+            if (count == 0) {
+                count = 1;
+                res = i;
+            }
+        }
+
+        // Step 2:
+        count = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[res] == a[i]) {
+                count++;
+            }
+        }
+
+        if (count <= (n / 2)) {
+            return -1;
+        } else
+            return a[res];
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 3, 3, 4, 2, 4, 4, 2, 4, 4 };
+        System.out.println(output(a));
+    }
+}
