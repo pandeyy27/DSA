335ee7744994364f91847f9ce362536fe5bc600f
Shubham <shubhampandeyy27@gmail.com>
2022-12-29 15:44:34 +0530

    first commit

---
+++ 04 - Mathematics/palindromeNumber.java
@@ -0,0 +1,19 @@
+// WAP to check palindrome of a number
+
+class palindromeNumber {
+    public static boolean palindromeNumbers(int n) {
+        int rev = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            int ld = temp % 10;
+            rev = rev * 10 + ld;
+            temp /= 10;
+        }
+        return n == rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(palindromeNumbers(343));
+    }
+}
