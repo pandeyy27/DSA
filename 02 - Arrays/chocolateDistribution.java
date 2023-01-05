ab90e9b4bd717a09ed2ac7da3be721e185f5af8e
Shubham <shubhampandeyy27@gmail.com>
2023-01-05 19:04:35 +0530

    updated the code

--- 02 - Arrays/chocolateDistribution.java
+++ 02 - Arrays/chocolateDistribution.java
@@ -17,27 +17,25 @@ import java.util.Arrays;
 
 public class chocolateDistribution {
     static int findMinDiff(int arr[], int n, int m) {
+
         Arrays.sort(arr);
+        int ans = Integer.MAX_VALUE;
 
-        if (m == 0 || n == 0)
-            return 0;
+        for (int i = 0; i < n - m; i++) {
+            int minw = arr[i];
+            int maxw = arr[i + m - 1];
+            int gap = maxw - minw;
 
-        if (n < m) // Number of students cannot be more than number of packets
-            return -1;
-
-        int min_diff = Integer.MAX_VALUE;
-
-        for (int i = 0; i + m - 1 < n; i++) {
-            int diff = arr[i + m - 1] - arr[i];
-            if (diff < min_diff)
-                min_diff = diff;
+            if (gap < ans) {
+                ans = gap;
+            }
         }
-        return min_diff;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int arr[] = { 12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 48, 43, 50 };
-        int m = 7;
+        int arr[] = {7, 3, 2, 4, 9, 12, 56};
+        int m = 3;
         int n = arr.length;
         System.out.println("Minimum difference is " + findMinDiff(arr, n, m));
     }
