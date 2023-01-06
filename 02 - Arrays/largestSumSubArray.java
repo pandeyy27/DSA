d6bae67fe9d28d0062a4c1f2904ada961f2856d7
Shubham <shubhampandeyy27@gmail.com>
2023-01-06 18:25:22 +0530

    updated the code

--- 02 - Arrays/largestSumSubArray.java
+++ 02 - Arrays/largestSumSubArray.java
@@ -5,21 +5,32 @@
 class largestSumSubArray {
 
     static int largestSubArray(int a[]) {
-        int curSum = 0;
-        int maxSum = Integer.MIN_VALUE;
+        // int curSum = 0;
+        // int maxSum = Integer.MIN_VALUE;
 
+        // for (int i = 0; i < a.length; i++) {
+        // curSum += a[i];
+        // if (maxSum < curSum)
+        // maxSum = curSum;
+        // if (curSum < 0)
+        // curSum = 0;
+        // }
+
+        int current_sum = 0, max_sum = Integer.MIN_VALUE;
         for (int i = 0; i < a.length; i++) {
-            curSum += a[i];
-            if (maxSum < curSum)
-                maxSum = curSum;
-            if (curSum < 0)
-                curSum = 0;
+            current_sum += a[i];
+            max_sum = Math.max(max_sum, current_sum);
+            if (current_sum <= 0) {
+                current_sum = 0;
+            }
         }
-        return maxSum;
+        return max_sum;
+
+        // return maxSum;
     }
 
     public static void main(String[] args) {
-        int[] a = { -2, 7, -6, 4, 1, -3, 8 };
+        int[] a = { -2, -4, -3, -6, -5 };
         System.out.println("Largest sum is " + largestSubArray(a));
     }
 
