b5e09b06e114a7bee291ea6dca36a9587c8c0ce9
Shubham <shubhampandeyy27@gmail.com>
2023-01-07 17:53:17 +0530

    updated the code

--- 02 - Arrays/minSumSubArray.java
+++ 02 - Arrays/minSumSubArray.java
@@ -3,22 +3,33 @@
 // Use Kadane's Algorithm
 
 public class minSumSubArray {
-    static int largestSubArray(int a[]) {
-        int curSum = 0;
-        int minSum = Integer.MAX_VALUE;
+    static int subArray(int a[]) {
+        // int curSum = 0;
+        // int minSum = Integer.MAX_VALUE;
 
+        // for (int i = 0; i < a.length; i++) {
+        // curSum += a[i];
+        // if (minSum > curSum)
+        // minSum = curSum;
+        // if (curSum > 0)
+        // curSum = 0;
+        // }
+
+        int current_sum = 0, min_sum = Integer.MAX_VALUE;
         for (int i = 0; i < a.length; i++) {
-            curSum += a[i];
-            if (minSum > curSum)
-                minSum = curSum;
-            if (curSum > 0)
-                curSum = 0;
+            current_sum += a[i];
+            min_sum = Math.min(min_sum, current_sum);
+            if (current_sum >= 0) {
+                current_sum = 0;
+            }
         }
-        return minSum;
+        return min_sum;
+
+        // return minSum;
     }
 
     public static void main(String[] args) {
-        int[] a = { -4, 5, -1, -6, 8 };
-        System.out.println("Largest sum is " + largestSubArray(a));
+        int[] a = { 3, -4, 2, -3, -1, 7, -5 };
+        System.out.println("Minimum sum is " + subArray(a));
     }
 }
